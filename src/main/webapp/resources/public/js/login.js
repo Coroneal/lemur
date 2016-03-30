7d8b745040f864eb5fcda945ce9f01feac969e6b
Coroneal <kulig.slawomir@gmail.com>
2016-03-30 20:57:28 +0200

    background

--- src/main/webapp/resources/public/js/login.js
+++ src/main/webapp/resources/public/js/login.js
@@ -1,15 +1,32 @@
 angular.module('loginApp', ['common', 'spring-security-csrf-token-interceptor','ngMaterial', 'ngMessages'])
     .config(function($mdThemingProvider, $mdIconProvider){
 
+            $mdThemingProvider.definePalette('lemurPalette', {
+                '50': '#f9fbe7',
+                '100': '#f0f4c3',
+                '200': '#e6ee9c',
+                '300': '#dce775',
+                '400': '#d4e157',
+                '500': '#cddc39',
+                '600': '#c0ca33',
+                '700': '#afb42b',
+                '800': '#9e9d24',
+                '900': '#827717',
+                'A100': '#f4ff81',
+                'A200': '#eeff41',
+                'A400': '#c6ff00',
+                'A700': '#aeea00',
+                'contrastDefaultColor': 'dark',    // whether, by default, text (contrast)
+                                                    // on this palette should be dark or light
+                'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                    '200', '300', '400', 'A100'],
+                'contrastDefaultColor': 'dark',
+                'contrastLightColors': '900',       // could also specify this if default was 'dark'
+                'contrastStrongLightColors': '900'
+            });
+
         $mdThemingProvider.theme('default')
-            .primaryPalette('grey', {
-                'default': '900',
-                'hue-2': '500'
-            })
-            //.backgroundPalette('grey', {
-            //    'default': '200'
-            //})
-            .accentPalette('orange');
+            .primaryPalette('lemurPalette');
     })
     .controller('LoginCtrl', ['$scope', '$http', function ($scope, $http) {
 
